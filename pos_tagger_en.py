dab39e41d8ae95c28a6005f136f1499996ade084
muga <muga.y.j3039@gmail.com>
2016-06-20 16:46:46 +0900

    spell change in the documentation says tag_text should be used.

--- pos_tagger_en.py
+++ pos_tagger_en.py
@@ -2,7 +2,7 @@ import treetaggerwrapper
 
 def main():
 	tagger = treetaggerwrapper.TreeTagger(TAGLANG='en', TAGDIR='/home/nak/muga/Downloads/')
-	tags = tagger.TagText(u"I am going to play tennis.")
+	tags = tagger.tag_text(u"I am going to play tennis.")
 	#must use unicode string e.g. 'u***'
 	for tag in tags:
 		print tag
