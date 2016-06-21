61ce298932503ca5e9e9502f91a5de3f8a025433
muga <muga.y.j3039@gmail.com>
2016-06-21 15:05:07 +0900

    add a function that reads a CSV file still doesn't work right

--- pos_tagger_en.py
+++ pos_tagger_en.py
@@ -4,22 +4,47 @@ import treetaggerwrapper
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn import svm
 from sklearn.grid_search import GridSearchCV
+import csv
+def extractLabels(input_file):
+	csv_reader = csv.reader(input_file, delimiter=",", quotechar='"')
+	labels = []
+	data = []
+	for row in csv_reader:
+		labels.append(row[0])
+		data.append(row[5])
+		if row[0] == "4":
+			labels.append(1)
+		else:
+			labels.append(0)
+
+		csv_reader.next()
+		
+	return (labels, data)
+
 def main():
 	#read input file
-	file_name = "example.txt"
+	#extract sentences (tweets)
+	file_name = "csv_example.csv"
 	file_path = os.getcwd() + "/" + file_name
 	if os.path.isfile(file_path) == False:
 		print "the example file doesn't exist"
 		quit()
 
-	input = open(file_path, 'r')
-	#generate a matrix of toke counts
+	input_file = open(file_path, "rb")
+	#make a list of labels and data
+	lables = []
+	data = []
+	labels, data = extractLabels(input_file)
+	#generate a matrix of token counts
 	count_vectorizer = CountVectorizer()
-	feature_vectors = count_vectorizer.fit_transform(input)
+	#input = open(file_path, 'rb')
+	#make a list of sentences
+	feature_vectors = count_vectorizer.fit_transform(data)
 	vocabulary = count_vectorizer.get_feature_names()
+	"""
 	for word in vocabulary:
 		print word
-	
+	"""
 	#learning by svm
 	svm_tuned_parameters = [
 		{
@@ -37,7 +62,8 @@ def main():
 		verbose=3# output level of mid-result
 	)
 
-	gscv.fit(feature_vectors, [1,-1,1,1,1,-1,1,-1, 1)#test label
+	#make a list of labels
+	gscv.fit(feature_vectors, labels)#test label
 	svm_model = gscv.best_estimator_
 
 	print svm_model
