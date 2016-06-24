73874f23298f47e00746b267877af783c7acefa0
muga <muga.y.j3039@gmail.com>
2016-06-24 12:19:31 +0900

    introduced RandomForestClassifier for multiclass classification. some subtle changes like variable names

--- multiclass_svm.py
+++ multiclass_svm.py
@@ -4,56 +4,44 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn import svm
 from sklearn.grid_search import GridSearchCV
 import csv
-def extractLabels(input_file):
+from sklearn.ensemble import RandomForestClassifier
+
+def extractLabelAndData(input_file):
 	csv_reader = csv.reader(input_file, delimiter=",", quotechar='"')
 	labels = []
 	data = []
 	for row in csv_reader:
 		data.append(row[5])
 		if row[0] == "4":
-			labels.append(1)
+			labels.append(1) #positive
+		elif row[0] == "2":
+			labels.append(0.5) #neutral
 		else:
-			labels.append(0)
+			labels.append(0) #negative
 	return (labels, data)
 
 def main():
 	#extract sentences (tweets)
-	file_name = "csv_example.csv"
-	file_path = os.getcwd() + "/" + file_name
+	#file_path = os.getcwd() + "/training.1600000.processed.noemoticon.csv"
+	file_path = os.getcwd() + "/csv_example.csv"
+
 	if os.path.isfile(file_path) == False:
 		print "the example file doesn't exist"
 		quit()
 
 	input_file = open(file_path, "rb")
 	#make a list of labels and data
-	lables = []
+	labels = []
 	tweets = []
-	labels, tweets = extractLabels(input_file)
+	labels, tweets = extractLabelAndData(input_file)
+	print 'extracting data has done'
 	#generate a matrix of token counts
 	count_vectorizer = CountVectorizer()
 	feature_vectors = count_vectorizer.fit_transform(tweets)
 	#learning by svm
-	svm_tuned_parameters = [
-		{
-			'kernel':['rbf'],
-			'gamma':[2 ** n for n in range(-15, 3)],
-			'C':[2 ** n for n in range(-5, 15)]
-		}
-	]
-	
-	gscv = GridSearchCV(
-		svm.SVC(),
-		svm_tuned_parameters,
-		cv=5,#number of cross varidation
-		n_jobs=1,# #of parallel thread
-		verbose=3# output level of mid-result
-	)
-
-	#make a list of labels
-	gscv.fit(feature_vectors, labels)#test label
-	svm_model = gscv.best_estimator_
-
-	print svm_model
+	estimator = RandomForestClassifier()
+	estimator.fit(feature_vectors, labels)
+	print "learning has done"
 
 if __name__ == "__main__":
 	main()
