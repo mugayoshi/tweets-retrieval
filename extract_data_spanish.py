f1caaca68f6b466565eededdb2374365ad82046c
muga <muga.y.j3039@gmail.com>
2016-07-13 16:33:58 +0900

    change to be able to write data to a file

--- extract_data_spanish.py
+++ extract_data_spanish.py
@@ -12,30 +12,43 @@ def getSentimentData(data):
 		sentiment_value += value
 	return sentiment_value
 
+def getSentimentDataList(data):
+	sentiment_value = []
+	for gc in list(data):
+		l = [x for x in gc.itertext()]
+		sentiment_value = sentiment_value + l
+	
+	return sentiment_value
+
 def main():
-	output_file = open('tweet_data_es.csv', 'ab')
-	writer_output = csv.writer(output_file)
+	argvs = sys.argv
+	if len(argvs) < 2:
+		print 'please input the output file name'
+		quit()
+	file_name = argvs[1]
+	output_file = open(file_name, 'wb')
+	#output_file = open('tweet_data_es.csv', 'wb')
 
 	filepath = '/home/nak/muga/annotated_corpus/twitter/spanish/TASS/general-tweets-train-tagged.xml'
 	tree = ET.parse(filepath)
 	elem = tree.getroot()
 	for e in list(elem):
 		tweet = ''
-		sentiment_value = ''
-		for c in list(e):
+		sentiment_value = []
+		for c in list(e):#c corresponds to a tweet data
 			if c.tag == 'content':
 				tweet = c.text
 			elif c.tag == 'sentiments':
-				sentiment_value = getSentimentData(c)
-		
-		#quit()
+				sentiment_value = getSentimentDataList(c)
+
 		if tweet is None or sentiment_value is None:
 			continue
 		elif len(tweet) == 0:
 			continue
-		line = tweet + ', ' + sentiment_value 
-		print 'tweet: ' + tweet
-		print 'sentiment value: ' + sentiment_value
+		line = '"' + tweet + '", ' + ' '.join(sentiment_value) + '\n'
+		output_file.write(line.encode('utf-8'))#necessary to encode otherwise cannot write strings
+
+	output_file.close()
 
 if __name__ == "__main__":
 		main()
