6bd9b25237483e21fb6ac77f429885134c8d81b1
muga <muga.y.j3039@gmail.com>
2016-07-15 15:07:16 +0900

    changed to make training data for all files

--- extract_tsv_data.py
+++ extract_tsv_data.py
@@ -16,21 +16,21 @@ def replaceWithNum(filename, sentiment_str, sentiment_value):
 	return
 
 def main():
-	argvs = sys.argv
-	if len(argvs) < 2:
-		print 'please input the output file name'
-		quit()
-	output_file = argvs[1]
-	#output_file = open(file_name, 'wb')
-	corpus_file_path = "/home/nak/muga/annotated_corpus/twitter/SentimentDatasetDaiLabor/de_sentiment_agree2.tsv"
-	input_file = pd.read_csv(corpus_file_path, delimiter='\t')
-	input_file.to_csv(output_file)
+	corpus_path = '/home/nak/muga/annotated_corpus/twitter/SentimentDatasetDaiLabor/'
+	tsv_files = []
+	for f in os.listdir(corpus_path):
+		if f.endswith('.tsv'):
+			tsv_files.append(f)
+	for f in tsv_files:
+		file_path = corpus_path + f
+		input_file = pd.read_csv(file_path, delimiter='\t')
+		output_file = f.split('.')[0] + '_training_data.csv'
+		input_file.to_csv(output_file)
+		
+		replaceWithNum(output_file, 'positive', '0')
+		replaceWithNum(output_file, 'negative', '1')
+		replaceWithNum(output_file, 'neutral', '2')
+		replaceWithNum(output_file, 'na', '3')
 	
-	replaceWithNum(output_file, 'positive', '0')
-	replaceWithNum(output_file, 'negative', '1')
-	replaceWithNum(output_file, 'neutral', '2')
-	replaceWithNum(output_file, 'na', '3')
-	
-
 if __name__ == '__main__':
 	main()
