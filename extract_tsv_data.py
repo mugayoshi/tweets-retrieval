e6425fae9090459ea202ed8cd1a3524592d20160
muga <muga.y.j3039@gmail.com>
2016-07-15 12:05:34 +0900

    changed the delimiter parameter to print propoerly

--- extract_tsv_data.py
+++ extract_tsv_data.py
@@ -4,8 +4,9 @@ import pandas as pd
 
 def main():
 	corpus_file_path = "/home/nak/muga/annotated_corpus/twitter/SentimentDatasetDaiLabor/de_sentiment_agree2.tsv"
-	input_file = pd.read_csv(corpus_file_path, delim_whitespace=True, names=['sentiment', 'tweet'])
-	input_file.to_csv('test_output_german.csv')
+	input_file = pd.read_csv(corpus_file_path, delimiter='\t', names=['sentiment', 'tweet'])
+	input_file.to_csv('test_output_german2.csv')
+	
 	
 
 if __name__ == '__main__':
