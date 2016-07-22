33ecc8d25c18e4a1dfc8e42369b8b7bf5d469526
muga <muga.y.j3039@gmail.com>
2016-07-22 10:45:32 +0900

    add an function that do classification. changed to classify files under 'data_for_test'

--- multiclass_random_forest.py
+++ multiclass_random_forest.py
@@ -5,6 +5,42 @@ from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.svm import SVC
 from sklearn.cross_validation import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import precision_score, recall_score, f1_score
+import os
+import sys
+import time
+
+def classification(filename):
+	labels, feature_vec = getFeatureVecAndLabel(filename)
+	data_train, data_test, label_train, label_test = train_test_split(feature_vec, labels, test_size=0.2)
+	print 'data extraction has done'
+	scores = ['accuracy', 'precision', 'recall']
+
+	date = time.strftime('%Y%m%d-%H%M%S')
+	out_file_name = filename.split('/')[-1].split('_')[0] + '_classsification_result_' + date + '.txt'
+	out_file_path = os.getcwd() + "/" + out_file_name
+
+	out = open(out_file_path, 'a')
+	#pp = pprint.PrettyPrinter(indent=4)
+	for score in scores:
+		out.write('\n' + '-'*50)
+		out.write(score)
+		out.write('-'*50)
+		tuned_parameters = [{'n_estimators': [10, 30, 50, 70, 90, 110, 130, 150], 'max_features':['auto', 'sqrt', 'log2', None]}]
+		clf = GridSearchCV(RandomForestClassifier(), param_grid=tuned_parameters, cv=3, scoring=score, n_jobs=-1)
+		clf.fit(data_train, label_train)
+		#out.write(pp.pprint(clf.best_estimator_))
+		print clf.best_estimator_
+
+		y_true, y_pred = label_test, clf.predict(data_test)
+		#print classification_report(y_true, y_pred, )
+		out.write(classification_report(y_true, y_pred))
+
+		print 'loop for ' + score + ' has done\n' 
+	
+	out.close()
+	print "classification of " + filename + " has done" 
+	return
 
 def extractTweetAndLabel(filename):
 	input_file = open(filename, 'rb')
@@ -44,33 +80,16 @@ def getFeatureVecAndLabel(filename):
 	#vocabulary_training_data = count_vectorizer.get_feature_names()
 
 	return (labels, feature_vectors)
-	
+
 def main():
-	"""
-	file_train = '/home/nak/muga/twitter/training_data/de_sentiment_training_data.csv'
-	file_test = '/home/nak/muga/twitter/training_data/de_sentiment_agree3_training_data.csv'
-	label_train, data_train = getFeatureVecAndLabel(file_train)
-	label_test, data_test = getFeatureVecAndLabel(file_test)
-	"""
-	file_input = '/home/nak/muga/twitter/train_data/de_sentiment_agree2_training_data.csv' #1720 data
-	labels, feature_vec = getFeatureVecAndLabel(file_input)
-	data_train, data_test, label_train, label_test = train_test_split(feature_vec, labels, test_size=0.2)
-	print 'data extraction has done'
-	scores = ['accuracy', 'precision', 'recall']
-	for score in scores:
-		print '\n' + '-'*50
-		print score
-		print '-'*50
-		tuned_parameters = [{'n_estimators': [10, 30, 50, 70, 90, 110, 130, 150], 'max_features':['auto', 'sqrt', 'log2', None]}]
-		clf = GridSearchCV(RandomForestClassifier(), param_grid=tuned_parameters, cv=3, scoring=score, n_jobs=-1)
-		clf.fit(data_train, label_train)
-		print clf.best_estimator_
 
-		y_true, y_pred = label_test, clf.predict(data_test)
-		print classification_report(y_true, y_pred)
-
-
-
+	train_datas_path = '/home/nak/muga/twitter/data_for_test/'
+	train_datas = []
+	for f in os.listdir(train_datas_path):
+		if f.endswith('.csv') and 'sentiment' in f:
+			train_datas.append(train_datas_path + f)
+	for f in train_datas:
+		classification(f)
 
 
 if __name__ == "__main__":
