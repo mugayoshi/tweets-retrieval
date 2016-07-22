8581e7c80b03d76ca7409728c9354e739cf0f98f
muga <muga.y.j3039@gmail.com>
2016-07-22 11:27:23 +0900

    changed the replacement function

--- extract_tsv_data.py
+++ extract_tsv_data.py
@@ -4,17 +4,26 @@ import sys
 import pandas as pd
 import shutil
 
-def replaceWithNum(filename, sentiment_str, sentiment_value):
-	with open(filename) as f:
-		s = f.read()
-		if sentiment_str not in s:
-			print sentiment_str + ' is not found'
-			return
-	
-	with open(filename, 'w') as f:
-		s = s.replace(sentiment_str, sentiment_value)
-		f.write(s)
-	
+def replaceSentimentValueWithNum(src, dest):
+	csv_input = open(src, "rb")
+	reader = csv.reader(csv_input)
+	csv_output = open(dest, 'wb')
+	writer = csv.writer(csv_output, delimiter=',', lineterminator='\n', quotechar='"', quoting=csv.QUOTE_ALL)
+	#header = next(reader)
+	for row in reader:
+		if row[1] == 'positive':
+			row[1] = 0
+		elif row[1] == 'negative':
+			row[1] = 1
+		elif row[1] == 'neutral':
+			row[1] = 2
+		elif row[1] == 'na':
+			row[1] = 3
+		
+		writer.writerow(row)
+	csv_input.close()
+	csv_output.close()
+
 	return
 
 def main():
@@ -28,24 +37,12 @@ def main():
 		input_file = pd.read_csv(file_path, delimiter='\t')
 		output_file = f.split('.')[0] + '_training_data.csv'
 		input_file.to_csv(output_file)
-		
-		replaceWithNum(output_file, 'positive', '0')
-		replaceWithNum(output_file, 'negative', '1')
-		replaceWithNum(output_file, 'neutral', '2')
-		replaceWithNum(output_file, 'na', '3')
-		
-		src = '/home/nak/muga/twitter/' + output_file
-		dest = '/home/nak/muga/twitter/train_data/' + output_file
+
 		#add "" to each item in a row
-		csv_input = open(src, "rb")
-		reader = csv.reader(csv_input)
-		csv_output = open(dest, 'wb')
-		writer = csv.writer(csv_output, delimiter=',', lineterminator='\n', quotechar='"', quoting=csv.QUOTE_ALL)
-		header = next(reader)
-		for row in reader:
-			writer.writerow(row)
-		csv_input.close()
-		csv_output.close()
+		src = '/home/nak/muga/twitter/' + output_file
+		dest = '/home/nak/muga/twitter/train_data2/' + output_file
+		replaceSentimentValueWithNum(src,dest)
+		
 		os.remove(src)
 		
 if __name__ == '__main__':
