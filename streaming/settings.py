2ffa997f057780130d52f2f2c3d11cd34cf224f9
adamoudad <adam.oudad@gmail.com>
2016-09-27 15:53:15 +0900

    Changed  to my personal twitter credentials

--- streaming/settings.py
+++ streaming/settings.py
@@ -1,8 +1,3 @@
-
-#Variables that contains the user credentials to access Twitter API 
-ACCESS_TOKEN = "574115777-FpPvFcducoKtQLrNGrnIUh7BgKKcciUoa8En9L5Q"
-ACCESS_TOKEN_SECRET = "8owLiHDqv8YplH5zriQ4MO0x5QocPT4ywpDtFNK34OV6W"
-CONSUMER_KEY = "yh0ltpdidxnb4y10h1zUOmz20"
-CONSUMER_SECRET = "XjJiiuHV7SWdYEUOuzTcFhLef0bmawoAJSisKM52pApd6gfWho"
+from credentials import ACCESS_TOKEN, ACCESS_TOKEN_SECRET, CONSUMER_KEY, CONSUMER_SECRET
 
 data = "../data/"
