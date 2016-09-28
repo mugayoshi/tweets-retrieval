033cfc5d0b5c1dcacc5c42e7195a875a73678218
adamoudad <adam.oudad@gmail.com>
2016-09-28 16:03:58 +0900

    Added scripts for processing tweets into a one tweet per line type of file

--- processing/twitter_processing.py
+++ processing/twitter_processing.py
@@ -1,18 +1,28 @@
+import sys
+import codecs
 import json
 import pandas as pd
 # import matplotlib.pyplot as plt
 
-data_path = "../data/raw.json"
-
-output = []
-data_file = open(data_path, "r")
-for line in data_file:
-    try:
-        tweet = json.loads(line)
-        print tweet['text']
-        print "----------------------------------------"
-        output.append(tweet)
-    except:
-        continue
-tweets = pd.DataFrame(output)
+if __name__ == "__main__":
+    if len(sys.argv) > 2:
+        json_path = sys.argv[1]
+        output_path = sys.argv[2]
+    else:
+        print "No raw data path and output path specified in arguments."
+        exit()
+    output = []
+    json_file = open(json_path, "r")
+    for line in json_file:
+        try:
+            tweet = json.loads(line)
+            output.append(tweet['text'].replace('\n',''))
+        except:
+            continue
+    # tweets = pd.DataFrame(output)
+    print "ok"
+    output_file = codecs.open(output_path, "w+", encoding="utf-8")
+    for tweet in output:
+        output_file.write("%s\n" % tweet)
+        
 
