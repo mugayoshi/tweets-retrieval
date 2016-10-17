afa801b0842891bcaeed7693f86f8dee6c3c999e
adamoudad <adam.oudad@gmail.com>
2016-10-17 14:11:48 +0900

    Commit of last version of streaming module before removal

--- streaming/__init__.py
+++ streaming/__init__.py
@@ -4,16 +4,19 @@
 
 import sys
 
+from tweepy import API
 from tweepy import OAuthHandler
 from tweepy import Stream
+from tweepy import Cursor
 
 from streaming.tweet import Tweet
-from streaming.listener import Listener
+# from streaming.listener import Listener
 from streaming.credentials import ACCESS_TOKEN, ACCESS_TOKEN_SECRET, CONSUMER_KEY, CONSUMER_SECRET
 
 def stream(keywords=[],language='',output_file="",max_count=0):
+    api = API(auth,wait_on_rate_limit=True,wait_on_rate_limit_notify=True)
     # Init Tweet stream
-    listener = Listener(output_file,max_count)
+    listener = Listener(api,output_file,max_count)
     auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
     auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
     stream = Stream(auth, listener)
@@ -21,4 +24,3 @@ def stream(keywords=[],language='',output_file="",max_count=0):
     # Stream search results into the console
     stream.filter(languages=[language],track=keywords)
 
-    
--- streaming/listener.py
+++ streaming/listener.py
@@ -1,16 +1,18 @@
 import sys
 import io
+import time
 
 from tweepy.streaming import StreamListener
 
 from streaming.tweet import Tweet
 
 class Listener(StreamListener):
-    def __init__(self, output_file, max_count=0):
+    def __init__(self, api, output_file, max_count=0):
+        super(Listener, self).__init__(api)
         self.count = 0    # How many tweets have been streamed
         self.max_count = max_count # max tweets to stream. 0 for infinite
         self.output_file = output_file
-        self.debug_file = io.open("data/debug.txt","w")
+        self.debug_file = io.open("data/debug_with_api.txt","w")
     def on_data(self, data):
         print(data,file=self.debug_file)
         tweet = Tweet(data)
@@ -19,7 +21,23 @@ class Listener(StreamListener):
             print(",".join(tweet.hashtags)+"\t"+t,file=self.output_file)
             self.count += 1
             if self.count == self.max_count: exit()
+
+        # try:
+        #     tweet = Tweet(data)
+        #     t = tweet.preprocess()
+        #     if t and tweet.isTagged():
+        #         print(",".join(tweet.hashtags)+"\t"+t,file=self.output_file)
+        #         self.count += 1
+        #         if self.count == self.max_count: exit()
+        # except Exception as e:
+        #     print("Twitter restriction handled")
+        #     print("Waiting...")
+        #     time.sleep(30)
+        #     print("Ok, now streaming again")
+        #     pass
         return True
 
     def on_error(self, status):
-        print("Error:", status)
+        if status_code == 429:
+            return False
+        print("Error !!!", status)
