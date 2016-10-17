1322d9c7d121c3df63d8be9298b782b4107efb55
adamoudad <adam.oudad@gmail.com>
2016-10-17 14:07:50 +0900

    Added script for generating a dataset in train format from a dataset in test format in utils.py

--- processing/utils.py
+++ processing/utils.py
@@ -1,9 +1,19 @@
+import io
 import random
+import os
 
-def file_len(f):
-    """
-    Returns the number of line of a file
-    """
-    return sum(1 for line in f)
+from dataset import Dataset
 
-# def randomize(d,)
+def file_len(path):
+    with io.open(path, 'r', encoding='utf-8') as f:
+        return sum(1 for _ in f)
+
+
+if __name__ == "__main__":
+    data_path = "/home/local/data/examples/"
+    for dirname in os.listdir(data_path):
+        print(dirname)
+        d = Dataset(data_path, dirname)
+        if d.isTest():
+            print("Test is present, making train")
+            d.make_train()
