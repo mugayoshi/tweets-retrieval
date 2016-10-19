cebf898ce6fefbe47babd1c249c9c886aab070a2
adamoudad <adam.oudad@gmail.com>
2016-10-19 13:53:45 +0900

    Added data manipulation with pandas in Dataset class, such as shuffle and method. Rewrote split method, added method for loading dataset into dataframe

--- processing/dataset.py
+++ processing/dataset.py
@@ -1,14 +1,73 @@
 import os
 import io
 
+import pandas as pd
+from sklearn.model_selection import train_test_split
 # from plot import ..
 
 class Dataset:
-    def __init__(self,path, name, ext=".txt",train="train",test="test"):
+    def __init__(self,path, name, ext=".txt"):
         self.name = name
         self.ext = ext
         self.path = path + name + "/"
+        self.data = None        # Will contain temporarily loaded datasets
+
+    def __len__(self, dataset="all"):
+        '''
+        Returns the length of dataset
+        '''
+        with io.open(self.path + dataset + self.ext, 'r', encoding='utf-8') as f:
+            return sum(1 for _ in f)
+
+    def load(self,dataset="all"):
+        self.data = pd.read_csv(self.path + dataset + self.ext, sep='\t', header=None)
+    
+    def append(self, line, dataset):
+        '''
+        Appends line at the end of the dataset file
+        '''
+        with io.open(self.path + dataset + self.ext,'a', encoding='utf-8') as f:
+            print(line, file=f)
+
+    def shuffle(self, dataset="all"):
+        '''
+        Shuffle dataset and drops duplicates
+        '''
+        self.load(dataset)
+        # http://stackoverflow.com/questions/29576430/shuffle-dataframe-rows
+        self.data = self.data.drop_duplicates()
+        self.data = self.data.sample(frac=1).reset_index(drop=True)
+        self.data.to_csv(self.path + dataset + self.ext, sep='\t', header=False, index=False)
+        self.data = None
+    
+    def split(self,train_ratio=0.75):
+        '''
+        Splits data in all.txt into train and test datasets following ratio specified by train_ratio
+        '''
+        if self.isTrain() or self.isTest():
+            print("There is already a train or test dataset")
+        else:
+            self.load()
+            train, test = train_test_split(self.data, train_size=train_ratio)
+            train.loc[:,0] = train[0].apply(lambda s: s.split(',')[0]) # Gives a SettingWithCopyWarning, ... dont know why, might ask on stackoverflow
+            train.to_csv(self.path + "train" + self.ext,
+                         sep='\t', header=False, index=False)
+            test.to_csv(self.path + "test" + self.ext,
+                        sep='\t', header=False, index=False)
+            self.data = None
         
+    def isLoaded(self):
+        '''
+        Returns true if data attribute contains a dataset
+        '''
+        return (self.data is not None)
+            
+    def isAll(self):
+        '''
+        Checks whether all data is present or not
+        '''
+        return os.path.isfile(self.path + "all" + self.ext)
+
     def isTest(self):
         '''
         Checks whether the test data is present or not
@@ -20,38 +79,3 @@ class Dataset:
         Checks whether the train data is present or not
         '''
         return os.path.isfile(self.path + "train" + self.ext)
-    
-    def make_train(self):
-        '''
-        Makes train dataset from test
-        '''
-        if self.isTrain():
-            print("There is already a train dataset")
-        else:
-            with io.open(self.path + "test" + self.ext,'r', encoding='utf-8') as in_file, io.open(self.path + "train" + self.ext, 'w',encoding='utf-8') as out_file:
-                for in_line in in_file:
-                    (yc, Xc) = in_line.rstrip('\n').split('\t')
-                    print(yc.split(',')[0] + '\t' + Xc, file=out_file)
-
-    
-    def test_size(self,f):
-        '''
-        Returns the size of the test data
-        '''
-        return file_len(self.path + "test" + self.ext)
-
-    def train_size(self,f):
-        '''
-        Returns the size of the test data
-        '''
-        return file_len(self.path + "train" + self.ext)
-    
-  
-    # def randomize(d,)
-    # import pandas as pd
-    # # Retrieve data from raw file
-    # df = pd.DataFrame.from_csv(input['path'] + input['extension'], sep='\t')
-    # # Keep only tweets contents
-    # df = df["tweet"][6:]
-    # # Save in new file
-    # df.to_csv(output['path'] + output['extension'], index=False)
--- processing/utils.py
+++ processing/utils.py
@@ -4,16 +4,19 @@ import os
 
 from dataset import Dataset
 
-def file_len(path):
-    with io.open(path, 'r', encoding='utf-8') as f:
-        return sum(1 for _ in f)
-
-
 if __name__ == "__main__":
     data_path = "/home/local/data/examples/"
-    for dirname in os.listdir(data_path):
-        print(dirname)
-        d = Dataset(data_path, dirname)
-        if d.isTest():
-            print("Test is present, making train")
-            d.make_train()
+    # for dirname in os.listdir(data_path):
+    #     print(dirname)
+    #     d = Dataset(data_path, dirname)
+    #     if d.isTest():
+    #         print("Test is present, making train")
+    #         d.make_train()
+    d = Dataset(data_path, "19294-nobel_prize_cell_science_chemistry_research")
+    print("All,Test,Train:",d.isAll(), d.isTest(),d.isTrain())
+    d.shuffle()
+    d.split()
+    print("Total Length:", len(d))
+    print("Train Length:", d.__len__("train"))
+    print("Test Length:", d.__len__("test"))
+    
