fd3efe3f622d99383485e4a30507af53418d7efc
adamoudad <adam.oudad@gmail.com>
2016-10-20 15:43:09 +0900

    Cleaned utils.py, useless comments and tests of dataset class new methods

--- processing/utils.py
+++ processing/utils.py
@@ -1,22 +1,24 @@
 import io
 import random
-import os
+from os.path import join
 
 from dataset import Dataset
 
 if __name__ == "__main__":
-    data_path = "/home/local/data/examples/"
-    # for dirname in os.listdir(data_path):
-    #     print(dirname)
-    #     d = Dataset(data_path, dirname)
-    #     if d.isTest():
-    #         print("Test is present, making train")
-    #         d.make_train()
-    d = Dataset(data_path, "19294-nobel_prize_cell_science_chemistry_research")
+    dataset_path = "/home/local/dataset/"
+    data_path = "/home/local/data/"
+    dataset_name = "twitter_dataset_1"
+
+    # Dataset creation
+    d = Dataset(dataset_path, dataset_name)
+    d.join(data_path + "examples")
+    print("Total Length:", len(d))
+
+    # Dataset manipulation
     print("All,Test,Train:",d.isAll(), d.isTest(),d.isTrain())
     d.shuffle()
     d.split()
     print("Total Length:", len(d))
     print("Train Length:", d.__len__("train"))
-    print("Test Length:", d.__len__("test"))
+    print("Test Length:", d.__len__("test"))   
     
