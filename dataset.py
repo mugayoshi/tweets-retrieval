716312311c766a4ed46bf26b6547a88089811671
adamoudad <adam.oudad@gmail.com>
2016-10-20 18:52:24 +0900

    Added methods and Cleaned dataset class, using os.path functions for file and dir manipulations.  - len method is able to return 0 if there is no file created yet.  - Wrote split kfold for cross validation.  - removed useless isTest isTrain isAll functions  - Wrote to_tsv method for handling dumping of DataFrame to tsv file

--- dataset.py
+++ dataset.py
@@ -1,5 +1,4 @@
-import os
-from os.path import isfile
+from os.path import isfile, normpath, join as joinpath
 from glob import glob           # search for pathnames function
 
 import pandas as pd
@@ -7,27 +6,29 @@ from sklearn.model_selection import train_test_split, KFold
 # Do stats, and split for cross validation
 
 class Dataset:
-    def __init__(self,path, name, ext=".txt"):
-        self.name = name
+    def __init__(self,path, name, ext="txt"):
+        self.name = normpath(name)
         self.ext = ext
-        self.path = path + name + "/"
+        self.path = joinpath(path,name) + '/{0}.{1}'
         self.data = None        # Will contain temporarily loaded datasets
 
     def __len__(self, dataset="all"):
         '''
         Returns the length of dataset
         '''
-        with open(self.path + dataset + self.ext, 'r', encoding='utf-8') as f:
+        if not isfile(self.path.format(dataset, self.ext)):
+            return 0
+        with open(self.path.format(dataset, self.ext), 'r', encoding='utf-8') as f:
             return sum(1 for _ in f)
 
     def load(self,dataset="all"):
-        self.data = pd.read_csv(self.path + dataset + self.ext, sep='\t', header=None)
+        self.data = pd.read_csv(self.path.format(dataset,self.ext), sep='\t', header=None)
     
     def append(self, line, dataset="all"):
         '''
         Appends line at the end of the dataset file
         '''
-        with open(self.path + dataset + self.ext,'a', encoding='utf-8') as f:
+        with open(self.path.format(dataset,self.ext),'a+', encoding='utf-8') as f:
             f.write(line)
 
     def shuffle(self, dataset="all"):
@@ -38,57 +39,54 @@ class Dataset:
         # http://stackoverflow.com/questions/29576430/shuffle-dataframe-rows
         self.data = self.data.drop_duplicates()
         self.data = self.data.sample(frac=1).reset_index(drop=True)
-        self.data.to_csv(self.path + dataset + self.ext, sep='\t', header=False, index=False)
+        self.data.to_csv(self.path.format(dataset,self.ext), sep='\t', header=False, index=False)
         self.data = None
     
-    def split(self,train_ratio=0.75, n_splits=1):
+    def split(self,train_ratio=0.75):
         '''
         Splits data in all.txt into train and test datasets following ratio specified by train_ratio
         '''
+        self.load()
+        train, test = train_test_split(self.data, train_size=train_ratio)
+        train.loc[:,0] = train[0].apply(lambda s: s.split(',')[0]) # Gives a SettingWithCopyWarning, ... dont know why, might ask on stackoverflow
+        self.to_tsv(train, "train")
+        self.to_tsv(test, "test")
+        self.data = None
+
+    def kfold_split(self, n_splits=2):
+        '''
+        Split with kfolds for cross validation
+        '''
+        self.load()
         kf = KFold(n_splits=n_splits)
-        for train_index, test_index in kf.split(X):
-            print("")
-        if self.isTrain() or self.isTest():
-            print("There is already a train or test dataset")
-        else:
-            self.load()
-            train, test = train_test_split(self.data, train_size=train_ratio)
+        count = 0
+        for train_index, test_index in kf.split(self.data):
+            train, test = self.data.iloc[train_index], self.data.iloc[test_index]
             train.loc[:,0] = train[0].apply(lambda s: s.split(',')[0]) # Gives a SettingWithCopyWarning, ... dont know why, might ask on stackoverflow
-            train.to_csv(self.path + "train" + self.ext,
-                         sep='\t', header=False, index=False)
-            test.to_csv(self.path + "test" + self.ext,
-                        sep='\t', header=False, index=False)
-            self.data = None
+            self.to_tsv(train, "train-"+str(count))
+            self.to_tsv(test, "test-"+str(count))
+            count+=1
+        self.data = None
 
     def join(self, data_path):
         '''
         Joins datasets from all files in data_path
         '''
-        for filename in glob(data_path + "*.txt"):
-            with open(data_path + filename) as f:
+        for file_path in glob(joinpath(data_path, "*.txt")):
+            with open(file_path) as f:
                 for l in f:
-                    d.append(l)
-            
+                    self.append(l)
+
+    def to_tsv(self, data=pd.DataFrame(), name="all"):
+        '''
+        Print dataframe into csv file
+        '''
+        if data.empty: data = self.data
+        data.to_csv(self.path.format(name,self.ext),
+                    sep='\t', header=False, index=False)
+                    
     def isLoaded(self):
         '''
         Returns true if data attribute contains a dataset
         '''
         return (self.data is not None)
-            
-    def isAll(self):
-        '''
-        Checks whether all data is present or not
-        '''
-        return isfile(self.path + "all" + self.ext)
-
-    def isTest(self):
-        '''
-        Checks whether the test data is present or not
-        '''
-        return isfile(self.path + "test" + self.ext)
-
-    def isTrain(self):
-        '''
-        Checks whether the train data is present or not
-        '''
-        return isfile(self.path + "train" + self.ext)
--- process.py
+++ process.py
@@ -1,24 +1,25 @@
 import io
 import random
-from os.path import join
+from os import path
 
 from dataset import Dataset
 
 if __name__ == "__main__":
-    dataset_path = "/home/local/dataset/"
-    data_path = "/home/local/data/"
-    dataset_name = "twitter_dataset_1"
+    dataset_path, data_path = "/home/local/dataset", "/home/local/data"
+    dataset_name, data_name = "joint_dataset", "tojoin"
 
     # Dataset creation
     d = Dataset(dataset_path, dataset_name)
-    d.join(data_path + "examples")
-    print("Total Length:", len(d))
+    # d.join(path.join(data_path,data_name))
+    # print("Total Length:", len(d))
 
-    # Dataset manipulation
-    print("All,Test,Train:",d.isAll(), d.isTest(),d.isTrain())
-    d.shuffle()
-    d.split()
+    # # Dataset manipulation
+    # d.shuffle()
+    d.split(0.67)
+    d.kfold_split(10)
     print("Total Length:", len(d))
     print("Train Length:", d.__len__("train"))
     print("Test Length:", d.__len__("test"))   
+    print("Train Length:", d.__len__("train-0"))
+    print("Test Length:", d.__len__("test-0"))   
     
