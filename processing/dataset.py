576cb0bb1e2a4bfc2eb0bd9cafdd6553e88f237b
adamoudad <adam.oudad@gmail.com>
2016-10-20 15:41:43 +0900

    Changed io.open to open function, wrote join function for joining datasets into one

--- processing/dataset.py
+++ processing/dataset.py
@@ -1,9 +1,10 @@
 import os
-import io
+from os.path import isfile
+from glob import glob           # search for pathnames function
 
 import pandas as pd
-from sklearn.model_selection import train_test_split
-# from plot import ..
+from sklearn.model_selection import train_test_split, KFold
+# Do stats, and split for cross validation
 
 class Dataset:
     def __init__(self,path, name, ext=".txt"):
@@ -16,18 +17,18 @@ class Dataset:
         '''
         Returns the length of dataset
         '''
-        with io.open(self.path + dataset + self.ext, 'r', encoding='utf-8') as f:
+        with open(self.path + dataset + self.ext, 'r', encoding='utf-8') as f:
             return sum(1 for _ in f)
 
     def load(self,dataset="all"):
         self.data = pd.read_csv(self.path + dataset + self.ext, sep='\t', header=None)
     
-    def append(self, line, dataset):
+    def append(self, line, dataset="all"):
         '''
         Appends line at the end of the dataset file
         '''
-        with io.open(self.path + dataset + self.ext,'a', encoding='utf-8') as f:
-            print(line, file=f)
+        with open(self.path + dataset + self.ext,'a', encoding='utf-8') as f:
+            f.write(line)
 
     def shuffle(self, dataset="all"):
         '''
@@ -40,10 +41,13 @@ class Dataset:
         self.data.to_csv(self.path + dataset + self.ext, sep='\t', header=False, index=False)
         self.data = None
     
-    def split(self,train_ratio=0.75):
+    def split(self,train_ratio=0.75, n_splits=1):
         '''
         Splits data in all.txt into train and test datasets following ratio specified by train_ratio
         '''
+        kf = KFold(n_splits=n_splits)
+        for train_index, test_index in kf.split(X):
+            print("")
         if self.isTrain() or self.isTest():
             print("There is already a train or test dataset")
         else:
@@ -55,7 +59,16 @@ class Dataset:
             test.to_csv(self.path + "test" + self.ext,
                         sep='\t', header=False, index=False)
             self.data = None
-        
+
+    def join(self, data_path):
+        '''
+        Joins datasets from all files in data_path
+        '''
+        for filename in glob(data_path + "*.txt"):
+            with open(data_path + filename) as f:
+                for l in f:
+                    d.append(l)
+            
     def isLoaded(self):
         '''
         Returns true if data attribute contains a dataset
@@ -66,16 +79,16 @@ class Dataset:
         '''
         Checks whether all data is present or not
         '''
-        return os.path.isfile(self.path + "all" + self.ext)
+        return isfile(self.path + "all" + self.ext)
 
     def isTest(self):
         '''
         Checks whether the test data is present or not
         '''
-        return os.path.isfile(self.path + "test" + self.ext)
+        return isfile(self.path + "test" + self.ext)
 
     def isTrain(self):
         '''
         Checks whether the train data is present or not
         '''
-        return os.path.isfile(self.path + "train" + self.ext)
+        return isfile(self.path + "train" + self.ext)
