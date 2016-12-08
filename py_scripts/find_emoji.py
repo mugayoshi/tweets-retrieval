ff705b25346360b3726c8189b64bc03ce01c2f21
mugayoshi <muga@sslab.ics.keio.ac.jp>
2016-12-08 17:53:15 +0900

    changed to be able to handle more languages

--- py_scripts/find_emoji.py
+++ py_scripts/find_emoji.py
@@ -1,10 +1,25 @@
 import sys
 import emoji
 
-def find_tweets_with_emoji(key_face, emoji_dict):
-	input_path = '/home/muga/twitter/classification_result/original_training_data/London/one_against_one/en/'
-	input_filename = 'result_accuracy_London_en_20Nov_uniq_02Dec20161614.txt'
-	input_file = open(input_path + input_filename)
+def find_tweets_with_emoji(key_face, emoji_dict, lang='en'):
+	if lang == 'en':
+		input_path_en = '/home/muga/twitter/classification_result/original_training_data/London/one_against_one/en/'
+		input_filename_en = 'result_accuracy_London_en_20Nov_uniq_02Dec20161614.txt'
+		input_file = open(input_path_en + input_filename_en)
+	elif lang == 'de':
+		input_path_de = '/home/muga/twitter/classification_result/original_training_data/Hamburg/one_against_one/de/'
+		input_filename_de = 'result_accuracy_Hamburg_de_10Nov_uniq_01Dec20161426.txt'
+		input_file = open(input_path_de + input_filename_de)
+	elif lang == 'fr':
+		input_path_fr = '/home/muga/twitter/classification_result/original_training_data/Paris/one_against_one/fr/'
+		input_filename_fr = 'result_accuracy_Paris_fr_20Nov_uniq_01Dec20161350.txt'
+		input_file = open(input_path_fr + input_filename_fr)
+	elif lang == 'es':
+		input_path_es = '/home/muga/twitter/classification_result/original_training_data/Barcelona/one_against_one/es/'
+		input_filename_es = 'result_accuracy_Barcelona_es_04Nov_23Nov20161621.txt'
+		input_file = open(input_path_es + input_filename_es)
+	
+
 	count = 0
 	for line in input_file.readlines():
 		#green_heart = emoji_dict[u':green_heart:']
@@ -17,8 +32,9 @@ def find_tweets_with_emoji(key_face, emoji_dict):
 			print line
 			count += 1
 	
-	print 'there are ' + str(count) + ' tweets with ' + key_face
+	print '\n' + '*' * 10 + str(count) + ' tweets with ' + key_face + '*' * 10 + '\n'
 	input_file.close()
+	return count
 
 def find_keys(key_word, emoji_dict):
 	uni_key_word = key_word.decode('utf-8')
@@ -35,9 +51,17 @@ def find_keys(key_word, emoji_dict):
 def main():
 	emoji_dict = emoji.EMOJI_UNICODE
 	if sys.argv[1]:
-		key_dic = find_keys(sys.argv[1], emoji_dict)
-		for key in key_dic:
-			find_tweets_with_emoji(key,emoji_dict) 
+		if len(sys.argv) > 2 and sys.argv[2] :
+			lang = sys.argv[2]
+		else:
+			lang = 'en'
+		key_list = find_keys(sys.argv[1], emoji_dict)
+		if len(key_list) == 0:
+			print 'no such key ' + sys.argv[1]
+		num_result = 0
+		for key in key_list:
+			num_result += find_tweets_with_emoji(key, emoji_dict, lang)
+		print '\n\n' + '#' * 10 + str(num_result) + ' for ' + sys.argv[1] + '#' * 10
 	else:
 		print 'keyword is necessary'
 		quit()
