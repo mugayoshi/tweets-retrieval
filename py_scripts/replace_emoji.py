7ce592de756c4e90509952d6710decc81f661a7d
mugayoshi <muga@sslab.ics.keio.ac.jp>
2016-12-09 12:55:04 +0900

    created replace_emoji.py

---
+++ py_scripts/replace_emoji.py
@@ -0,0 +1,87 @@
+import emoji
+import csv
+import common_functions_general as cfg
+import os
+import sys
+def make_dictionary(lang='en'):
+
+	dict_file = '/home/muga/twitter/emoji_dict_all_languages.csv'
+	
+	input_file = open(dict_file, 'rU')
+	csv_reader = csv.reader(input_file, delimiter=",")
+	dict = {}
+	#header = next(csv_reader)
+	for row in csv_reader:
+		#print row[0], row[1]
+		#print ', '.join(row)
+		#data.append(row[0]) #depends on file
+		if lang == 'en':
+			dict[row[0]] = row[1]
+		elif lang == 'fr':
+			dict[row[0]] = row[2]
+		elif lang == 'de':
+			dict[row[0]] = row[3]
+		elif lang == 'es':
+			dict[row[0]] = row[4]
+		elif lang == 'pt':
+			dict[row[0]] = row[5]
+		
+	
+
+	return dict
+
+def replace_emoji_with_str(emoji_descp_dict, test_data, outfile):
+	emoji_dict = emoji.EMOJI_UNICODE
+	valid_keys = []
+	for key in emoji_dict.keys():
+		if key in emoji_descp_dict.keys():
+			valid_keys.append(key)
+
+	for line in test_data.readlines():
+		output_line = line
+		changed = False
+		for key in valid_keys:
+			emoji_code = emoji_dict[key]
+			emoji_descp = emoji_descp_dict[key]
+			#print 'emoji code ' + str(type(emoji_code)) + ' line ' + str(type(line.decode('utf-8')))
+			try:
+				if emoji_code in line.decode('utf-8'):
+					replaced_with = ' { ' + emoji_descp + ' } ' 
+					output_line = output_line.replace(emoji_code.encode('utf-8'), replaced_with)
+					print 'replaced ' + key.encode('utf-8') + ' ' + emoji_code.encode('utf-8') + ' ' + emoji_descp
+					changed = True
+			except:
+				continue
+		if changed:
+			print line + ' ---> ' + output_line
+		outfile.write(output_line)
+	return
+def main():
+	if len(sys.argv) < 3:
+		print 'must input city name and language if necessary.'
+		quit()
+	city = sys.argv[1]
+	lang = sys.argv[2]
+	emoji_descp_dict = make_dictionary(lang)
+	test_data_path = '/home/muga/twitter/test_data/retrieved_data/' + city + '/'
+	test_data_list = []
+	for f in os.listdir(test_data_path):
+		if lang in f and city in f:
+			test_data_list.append(f)
+	#test_data = test_data_path + 'London_en_20Nov_uniq.csv'
+	out_path = '/home/muga/twitter/test_data/emoji_replaced/' + city + '/'
+	cfg.validate_directory(out_path, True)
+	for f in test_data_list:
+		out_filename = f
+		print out_filename
+		input_file = open(test_data_path + f, 'r')
+		output_file = open(out_path + f, 'w')
+		replace_emoji_with_str(emoji_descp_dict, input_file, output_file)
+		input_file.close()
+		output_file.close()
+
+	return
+
+if __name__ == "__main__":
+	main()
+
